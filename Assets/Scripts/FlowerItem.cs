8d5196f40f5e0b4906ae599dc2e43077174d7fb8
chhannz <chhannz@gmail.com>
2026-06-18 17:11:20 +0700

    update18juni

--- Assets/Scripts/FlowerItem.cs
+++ Assets/Scripts/FlowerItem.cs
@@ -1,16 +1,22 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 [RequireComponent(typeof(SpriteRenderer))]
 [RequireComponent(typeof(BoxCollider2D))]
 
-public class FlowerItem : MonoBehaviour
+public class FlowerItem : MonoBehaviour, IPointerClickHandler
 {
     [SerializeField] private FlowerData flowerData;
     private SpriteRenderer spriteRenderer;
+
+    [SerializeField] private float targetSize = 1f;
+
+    private BoxCollider2D boxCollider;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
+        boxCollider =  GetComponent<BoxCollider2D>();
     }
 
     void Start()
@@ -19,6 +25,8 @@ public class FlowerItem : MonoBehaviour
         {
             spriteRenderer.sprite = flowerData.FlowerSprite;
             gameObject.name = flowerData.FlowerName;
+            AdjustScale();
+            boxCollider.size = spriteRenderer.sprite.bounds.size;
         }
         else
         {
@@ -26,9 +34,24 @@ public class FlowerItem : MonoBehaviour
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    void AdjustScale()
     {
-        
+        Vector2 spriteSize = spriteRenderer.sprite.bounds.size;
+        float largestSide = Mathf.Max(spriteSize.x, spriteSize.y);
+        float scaleFactor = targetSize / largestSide;
+        transform.localScale = new Vector3(scaleFactor, scaleFactor, 1f);
+    }
+    
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Debug.Log("Clicked");
+        if (BouquetManager.Instance != null)
+        {
+            BouquetManager.Instance.AddFlower(this);
+        }
+        else
+        {
+            Debug.Log("No bouquet Manager");
+        }
     }
 }
